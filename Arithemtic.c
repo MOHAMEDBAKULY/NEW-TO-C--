d2aaea6113a2924d60c385dfefb66c4c32333719
MOHAMED BAKULY <119895383+MOHAMEDBAKULY@users.noreply.github.com>
2024-02-08 14:43:41 +0300

    Introduction to C

---
+++ Arithemtic.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+   int x, y, results;
+   x = 50;
+   y = 20;
+
+
+    // results =  x + y;
+    // results =  x - y;
+    // results =  x * y;
+    // results =  x % y;
+    results =  x / y;
+
+    // printf("sum of %d and %d is %d, x, y, results");
+    // printf("%d + %d = %d, x, y, results");
+    // printf("x + y = %d", results);
+    // printf("\n x - y = %d", results);
+    // printf("\n x * y = %d", results);
+    // printf("\n x % y = %d", results);
+    printf("\n x / y = %d", results);
+
+
+
+    return 0;
+}
---
+++ hello.c
@@ -0,0 +1,15 @@
+// #include <stdio.h>
+// int main() {
+// 	// printf() displays the string inside quotation
+// 	printf("Hello, World!");
+// 	printf("\n Hello in JKUAT");
+
+// 	int number;
+
+// 	printf("\n Enter a number ");
+
+// 	scanf(" %d", &number);
+
+// 	printf("The number Entered us %d", number);
+// 	return 0;
+// }
