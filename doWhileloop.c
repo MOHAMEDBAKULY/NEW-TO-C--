4bef7fa9c1a1d85d90de6cfca10e3e47fa521cc2
MOHAMED BAKULY <119895383+MOHAMEDBAKULY@users.noreply.github.com>
2024-02-28 21:20:31 +0300

    DO while loops in C

---
+++ doWhileloop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    int i = 4;
+
+    do{
+        printf("Loops are really cool when you understand them\n");
+
+        i++;
+    } while(i < 14);
+
+return 0;
+}
